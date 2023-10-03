159a797ca971dbdd87d97527e18386fe23e468f5
Mohammad Mahdi <agamohammadzadeh@gmail.com>
2023-10-03 14:14:29 +0330

    finish left navbar

--- config/db-config.php
+++ config/db-config.php
@@ -1,7 +1,11 @@
 <?php 
 
 try {
+    
     $conn = new PDO("mysql:host=localhost;dbname=azixaaaw_webmusic;charset=utf8" , 'azixaaaw_Mohammad' , '@13Mohammad86@');    
+
+    $conn->setAttribute(PDO::ATTR_ERRMODE , PDO::ERRMODE_EXCEPTION);
+
     
 } catch (PDOException $e) {
 
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php 
 
-// include __DIR__ . '/config/db-config.php';
+include __DIR__ . '/config/db-config.php';
 
 
 ?>
@@ -15,8 +15,8 @@
         href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css"
     />
     <link rel="stylesheet" href="/vendor/Font-Awsome/css/all.min.css">
-    <link rel="stylesheet" href="/css/fonts.css">
     <link rel="stylesheet" href="/css/style.css">
+    <script src="/vendor/ID3/dist/id3-minimized.js"></script>
     <title> Web Music </title>
 </head>
 <body>
@@ -24,14 +24,115 @@
 
         <div class="left-navbar">
 
-            
+            <div class="left-navbar__icons-main-container">
+
+                <div class="left-navbar__logo-container">
+
+                    <div class="left-navbar__logo-background">
+
+                        <div class="left-navbar__logo-icon">
+
+                            <i class="fi fi-rr-music-alt"></i>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+                <div class="left-navbar__icons">
+
+                    <div class="left-navbar__icons-container">
+ 
+                        <div class="left-navbar__icon-container home">
+
+                            
+                            <a href="" class="left-navbar__icon-link" title="Home">
+                                
+                                <i class="fi fi-rr-home"></i>
+                            
+                            </a>
+
+                        </div>
+
+                        <div class="left-navbar__icon-container add">
+
+                            <a href="#" class="left-navbar__icon-link" title="Add ...">
+                                
+                                <i class="fi fi-rr-square-plus"></i>
+                            
+                            </a>
+
+                        </div>
+
+                        <div class="left-navbar__icon-container wishlist">
+
+                            <a href="#" class="left-navbar__icon-link" title="WishList">
+                                
+                                <i class="fi fi-rr-heart"></i>
+                            
+                            </a>
+
+                        </div>
+
+                        <div class="left-navbar__icon-container musics">
+
+                            <a href="#" class="left-navbar__icon-link" title="Musics">
+                                
+                                <i class="fi fi-rr-music-note"></i>
+                            
+                            </a>
+
+                        </div>
+
+                        <div class="left-navbar__icon-container albums">
+
+                            <a href="#" class="left-navbar__icon-link" title="Albums">
+                                
+                                <i class="fi fi-rr-album-collection" ></i>
+                            
+                            </a>
+
+                        </div>
+
+                        <div class="left-navbar__icon-container artists">
+
+                            <a href="#" class="left-navbar__icon-link" title="Artists">
+                                
+                                <i class="fi fi-rr-microphone-alt" ></i>
+                            
+                            </a>
+
+                        </div>
+
+                        <div class="left-navbar__icon-container setting">
+
+                            <a href="#" class="left-navbar__icon-link"  title="Setting">
+                                
+                                <i class="fi fi-rr-user-gear"></i>
+                            
+                            </a>
+
+                        </div>
+
+                    </div>
+
+
+                </div>
+
+            </div>
 
         </div>
 
 
-        <main class="main-content">
+        <div class="right-container">
 
-        </main>
+
+            <main class="main-content">
+                
+            </main>
+            
+        </div>
 
     </div>
     <script src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js"></script>
